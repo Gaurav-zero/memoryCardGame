665885c04541cc6c2733c22d192465ee1d873516
Gaurav <183895839+Gaurav-zero@users.noreply.github.com>
2026-03-26 22:15:32 +0530

    initial commit

---
+++ src/components/scorecard.jsx
@@ -0,0 +1,10 @@
+function scoreCard(currScore, bestScore){
+    return (
+        <div>
+            <p>"Score:" {currScore}</p>
+            <p>"Best Score:" {bestScore}</p>
+        </div>
+    )
+}
+
+export {scoreCard};
